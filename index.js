2afccead903b876173a451dd4720e67374a961c3
Jhonny Cordova <jhonny.cordova@dsarhoya.cl>
2021-06-24 11:25:16 -0400

    connecting with mongo

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 dotenv.config();
 const app = express();
 
@@ -15,6 +16,10 @@ app.post('/', (req, res) => {
     console.log(req.body);
 });
 
-app.listen(3000, () => {
-    console.log("Listening on port 3000");
-});
+mongoose.set("useFindAndModify", false);
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
+    console.log('Mongo is connected');
+    app.listen(3000, () => {
+        console.log("Listening on port 3000");
+    });
+})
