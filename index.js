c75a5170472ed5b658c84b6052baf73d7c3c23c4
Jhonny Cordova <jhonny.cordova@dsarhoya.cl>
2021-06-24 14:10:25 -0400

    Usando NeDB para la base de datos

--- index.js
+++ index.js
@@ -1,25 +1,58 @@
 const express = require("express");
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
-dotenv.config();
+// const dotenv = require('dotenv');
+
+// dotenv.config();
 const app = express();
 
+var Datastore = require('nedb');
+var db = new Datastore({filename:'./data/database'});
+
 app.set("view engine", "ejs");
-app.use("/static", express.static("public"));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.render("todo");
+app.get('/tasks', (req, res) => {
+    db.find({}, function(err, records) {
+        res.status(200).json({
+            message: "Task listed!",
+            tasks: records 
+        });
+    });
 });
 
-app.post('/', (req, res) => {
-    console.log(req.body);
+app.post('/tasks', (req, res) => {
+    const task = { name: req.body.name };
+    db.insert(task, (err, record) => {
+        res.status(200).json({
+           message: "Task created!",
+           task: record 
+        });
+    })
 });
 
-mongoose.set("useFindAndModify", false);
-mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
-    console.log('Mongo is connected');
+app.delete('/tasks/:id', (req, res) => {
+    const taskId = req.params.id;
+    db.remove({ _id: taskId }, {}, function (err, numRemoved) {
+        res.status(200).json({
+            message: "Task removed!",
+        });
+    });
+});
+
+app.put('/tasks/:id', (req, res) => {
+    const taskId = req.params.id;
+    const task = { name: req.body.name };
+    db.update({ _id: taskId }, task, {}, function () {
+        res.status(204).json({
+            message: "Task updated!",
+        });
+    });
+});
+
+db.loadDatabase(function (err) {
     app.listen(3000, () => {
         console.log("Listening on port 3000");
     });
-})
+});
+
